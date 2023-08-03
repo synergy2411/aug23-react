b46b27b9bf0ee7e4269214f8bb6e9da828232000
Sumit Khandelwal <sumitkhandelwal@Sumits-MacBook-Air.local>
2023-08-03 20:54:22 +0530

    expense item added

--- first-app/src/components/Expeses/Expenses.jsx
+++ first-app/src/components/Expeses/Expenses.jsx
@@ -1,16 +1,36 @@
 import React from "react";
+import ExpenseItem from "./ExpenseItem/ExpenseItem";
 
 function Expenses() {
-  let expense = {
-    id: "e001",
-    title: "pot the plants",
-    amount: 199,
-    createdAt: new Date("Jan 21, 2023"),
-  };
+  let expenses = [
+    {
+      id: "e001",
+      title: "pot the plants",
+      amount: 199,
+      createdAt: new Date("Jan 21, 2022"),
+    },
+    {
+      id: "e002",
+      title: "renew car insurance",
+      amount: 399,
+      createdAt: new Date("Jul 2, 2023"),
+    },
+    {
+      id: "e003",
+      title: "buy the jeans",
+      amount: 99,
+      createdAt: new Date("Feb 10, 2023"),
+    },
+  ];
 
   return (
     <div className="container">
-      <h2 className="text-center">Expenses coming soon...</h2>
+      <h2 className="text-center">My Expenses App</h2>
+      <div className="row">
+        {expenses.map((expense) => (
+          <ExpenseItem key={expense.id} expense={expense} />
+        ))}
+      </div>
     </div>
   );
 }
