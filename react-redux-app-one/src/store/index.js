a56075b6b24fd90922cd509b5aece9bcdca6558e
Sumit Khandelwal <sumitkhandelwal@Sumits-MacBook-Air.local>
2023-08-09 21:01:45 +0530

    redux devtool added

--- react-redux-app-one/src/store/index.js
+++ react-redux-app-one/src/store/index.js
@@ -1,5 +1,5 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import rootReducer from "./reducers/root.reducer";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+// import rootReducer from "./reducers/root.reducer";
 import counterReducer from "./reducers/counter.reducer";
 import resultReducer from "./reducers/result.reducer";
 import logger from "./middlewares/logger";
@@ -7,12 +7,14 @@ import thunk from "redux-thunk";
 
 // const store = createStore(rootReducer)
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   combineReducers({
     ctr: counterReducer,
     res: resultReducer,
   }),
-  applyMiddleware(logger, thunk)
+  composeEnhancers(applyMiddleware(logger, thunk))
 );
 
 export default store;
