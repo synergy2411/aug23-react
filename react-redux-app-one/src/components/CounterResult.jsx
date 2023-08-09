a2da95a2cfff9350613631c3aa2d42909b7a4901
Sumit Khandelwal <sumitkhandelwal@Sumits-MacBook-Air.local>
2023-08-09 21:17:20 +0530

    class based comp with redux

--- react-redux-app-one/src/components/CounterResult.jsx
+++ react-redux-app-one/src/components/CounterResult.jsx
@@ -1,32 +1,90 @@
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+
+import { useDispatch, useSelector, connect } from "react-redux";
 import * as fromActions from "../store/actions/counter.action";
 
-export default function CounterResult() {
-  const dispatch = useDispatch();
-  const counter = useSelector((store) => store.ctr.counter);
-  const result = useSelector((store) => store.res.result);
+// export default function CounterResult() {
+//   const dispatch = useDispatch();
+//   const counter = useSelector((store) => store.ctr.counter);
+//   const result = useSelector((store) => store.res.result);
 
-  return (
-    <>
-      <hr />
-      <button
-        className="btn btn-danger btn-lg"
-        onClick={() => dispatch(fromActions.storeResult(counter))}
-      >
-        Store Result
-      </button>
+//   return (
+//     <>
+//       <hr />
+//       <button
+//         className="btn btn-danger btn-lg"
+//         onClick={() => dispatch(fromActions.storeResult(counter))}
+//       >
+//         Store Result
+//       </button>
 
-      <ul className="list-group">
-        {result.map((r, i) => (
-          <li
-            className="list-group-item"
-            key={i}
-            onClick={() => dispatch(fromActions.deleteResult(i))}
-          >
-            {r}{" "}
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+//       <ul className="list-group">
+//         {result.map((r, i) => (
+//           <li
+//             className="list-group-item"
+//             key={i}
+//             onClick={() => dispatch(fromActions.deleteResult(i))}
+//           >
+//             {r}{" "}
+//           </li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// }
+
+class CounterResult extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <hr />
+        <button
+          className="btn btn-danger btn-lg"
+          onClick={() => this.props.onStoreResult(this.props.counter)}
+        >
+          Store Result
+        </button>
+
+        <ul className="list-group">
+          {this.props.result.map((r, i) => (
+            <li
+              className="list-group-item"
+              key={i}
+              onClick={() => this.props.onDeleteResult(i)}
+            >
+              {r}{" "}
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
 }
+
+const mapStateToProps = (store) => {
+  return {
+    counter: store.ctr.counter,
+    result: store.res.result,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onStoreResult: (value) => {
+      dispatch(fromActions.storeResult(value));
+    },
+    onDeleteResult: (index) => {
+      dispatch(fromActions.deleteResult(index));
+    },
+  };
+};
+
+// const connectedFn = connect(mapStateToProps, mapDispatchToProps);
+
+// export default connectedFn(CounterResult);
+
+export default connect(mapStateToProps, mapDispatchToProps)(CounterResult);
